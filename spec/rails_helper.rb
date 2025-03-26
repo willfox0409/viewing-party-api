d6e38fd91b68c1347d4c6e884069088e259b7821
Will Fox <william.macdonald.fox@gmail.com>
2025-03-26 18:15:43 -0400

    chore: add require webmock to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'simplecov'
 SimpleCov.start
+
+require 'webmock/rspec'
+# WebMock.disable_net_connect!(allow_localhost: true)
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
