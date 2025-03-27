f3734feb63d09c75d9eb5e0d056bc85b3e2b28fe
Will Fox <william.macdonald.fox@gmail.com>
2025-03-27 19:30:00 -0400

    fix: update helper method name to match test

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::MoviesController < ApplicationController
 
   def index
-    movies = MovieService.get_top_rated
+    movies = MovieService.get_top_rated_movies
+    # puts MovieSerializer.new(movies).serializable_hash.to_json
     render json: MovieSerializer.new(movies)
   end
 end 
