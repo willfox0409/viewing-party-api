8c5b1c64c4e5cb9c98f90d7451aca334a6e4374f
Will Fox <william.macdonald.fox@gmail.com>
2025-03-29 11:15:47 -0400

    feat: add User model with AR associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :invitations
+  has_many :viewing_parties, through: :invitations 
+
   validates :name, presence: true
   validates :username, presence: true, uniqueness: true
   validates :password, presence: { require: true }
