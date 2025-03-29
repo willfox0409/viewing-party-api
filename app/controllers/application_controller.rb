090328a761b551bcaad41f92b08eb3263b1e1625
Will Fox <william.macdonald.fox@gmail.com>
2025-03-29 11:13:20 -0400

    chore: add rescue_froms to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable
+
+  private
+
+  def render_not_found(error)
+    render json: ErrorSerializer.format_error(error.message, 400), status: :bad_request
+  end
+  
+  def render_unprocessable(error)
+    render json: ErrorSerializer.format_error(error.message, 422), status: :unprocessable_entity
+  end
 end
