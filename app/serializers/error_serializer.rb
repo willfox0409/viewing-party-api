75c71165900248ec49123f320b32066ff7545fcb
Will Fox <william.macdonald.fox@gmail.com>
2025-03-30 17:20:21 -0400

    chore: add format_errors_array helper method in error serializer to account for simultaneous error messages

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -5,4 +5,13 @@ class ErrorSerializer
       status: status_code
     }
   end
+
+  def self.format_errors_array(messages, status_code) #creates an array if there are multiple errors 
+    {
+      errors: Array(messages).map do |msg|
+        { detail: msg }
+      end,
+      status: status_code
+    }
+  end
 end
