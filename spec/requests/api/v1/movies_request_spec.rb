6cb3707b795390e0562000d6aa5250b2c1b9ec36
Will Fox <william.macdonald.fox@gmail.com>
2025-03-30 22:55:00 -0400

    test: complete #show tests in movies request spec with sad path

--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -71,4 +71,85 @@ RSpec.describe "Movies endpoints", type: :request do
       end
     end
   end
+
+  describe "GET /api/v1/movies/:id" do
+    it "returns detailed info for a specific movie" do
+      movie_id = 278
+  
+      stub_request(:get, "https://api.themoviedb.org/3/movie/#{movie_id}")
+        .with(query: hash_including({}))
+        .to_return(status: 200, body: {
+          id: 278,
+          title: "The Shawshank Redemption",
+          release_date: "1994-09-23",
+          vote_average: 8.7,
+          runtime: 142,
+          genres: [{ name: "Drama" }, { name: "Crime" }],
+          overview: "Summary of the movie"
+        }.to_json, headers: { "Content-Type" => "application/json" })
+  
+      stub_request(:get, "https://api.themoviedb.org/3/movie/#{movie_id}/credits")
+        .with(query: hash_including({}))
+        .to_return(status: 200, body: {
+          cast: [
+            { character: "Andy Dufresne", name: "Tim Robbins" },
+            { character: "Red", name: "Morgan Freeman" }
+          ]
+        }.to_json, headers: { "Content-Type" => "application/json" })
+  
+      stub_request(:get, "https://api.themoviedb.org/3/movie/#{movie_id}/reviews")
+        .with(query: hash_including({}))
+        .to_return(status: 200, body: {
+          results: [
+            { author: "elshaarawy", content: "A classic." },
+            { author: "john_doe", content: "Loved it!" }
+          ]
+        }.to_json, headers: { "Content-Type" => "application/json" })
+  
+      get "/api/v1/movies/#{movie_id}"
+  
+      expect(response).to be_successful
+  
+      json = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(json[:data][:id]).to eq(movie_id.to_s)
+      expect(json[:data][:attributes][:title]).to eq("The Shawshank Redemption")
+      expect(json[:data][:attributes][:release_year]).to eq("1994")
+      expect(json[:data][:attributes][:runtime]).to eq("2 hours, 22 minutes")
+      expect(json[:data][:attributes][:genres]).to include("Drama", "Crime")
+      expect(json[:data][:attributes][:summary]).to be_a(String)
+      expect(json[:data][:attributes][:cast].first).to include(:character, :actor)
+      expect(json[:data][:attributes][:total_reviews]).to eq(2)
+      expect(json[:data][:attributes][:reviews].first).to include(:author, :review)
+    end
+
+    it "returns 404 if the movie is not found" do
+      invalid_id = 9999999
+  
+      stub_request(:get, "https://api.themoviedb.org/3/movie/#{invalid_id}")
+        .with(query: hash_including({}))
+        .to_return(status: 404, body: {
+          status_message: "The resource you requested could not be found."
+        }.to_json, headers: { "Content-Type" => "application/json" })
+
+      stub_request(:get, "https://api.themoviedb.org/3/movie/#{invalid_id}/credits")
+        .with(query: hash_including({}))
+        .to_return(status: 404, body: {
+          status_message: "The resource you requested could not be found."
+        }.to_json, headers: { "Content-Type" => "application/json" })
+      
+      stub_request(:get, "https://api.themoviedb.org/3/movie/#{invalid_id}/reviews")
+        .with(query: hash_including({}))
+        .to_return(status: 404, body: {
+          status_message: "The resource you requested could not be found."
+        }.to_json, headers: { "Content-Type" => "application/json" })
+  
+      get "/api/v1/movies/#{invalid_id}"
+  
+      expect(response).to have_http_status(:not_found)
+  
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:errors].first[:detail]).to match(/not found/i)
+    end
+  end
 end 
