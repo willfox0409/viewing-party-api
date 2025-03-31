39edfc38c3393be64a020b05294e7518501ef58c
Will Fox <william.macdonald.fox@gmail.com>
2025-03-31 17:13:20 -0400

    feat: add helper methods for filtering hosted parties and invited parties for a given user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :api_key
 
+  def hosted_parties #filters just the invitations where user is host
+    invitations.where(host: true).map do |invitation|  
+      invitation.viewing_party #get the viewing party for each of those invitations 
+    end
+
+  end
+
+  def invited_parties #filters the invitations where user isn't the host 
+    invitations.where(host: false).map do |invitation|  
+      invitation.viewing_party #get the viewng party for each of those invitations 
+    end
+  end
 end
