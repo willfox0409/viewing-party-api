592370990fb60eb03792a74027a89001a772559a
Will Fox <william.macdonald.fox@gmail.com>
2025-03-31 17:14:20 -0400

    chore: add show endpoint to users resource

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :index]
+      resources :users, only: [:create, :index, :show]
       resources :sessions, only: :create
       resources :movies, only: [:index, :show]
       resources :viewing_parties, only: :create do
